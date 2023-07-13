4eea48b1129794575865da3191a3921a26d8d966
YarynaFialko <92580927+YarynaFialko@users.noreply.github.com>
2023-07-13 19:39:10 +0300

    unnesessary libraries removed

--- Math Snake/Assets/Scripts/ParticleManager.cs
+++ Math Snake/Assets/Scripts/ParticleManager.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ParticleManager : MonoBehaviour
@@ -7,12 +5,6 @@ public class ParticleManager : MonoBehaviour
 
     public GameObject mathParticle;
     public GameObject hitParticle;
-
-
-    void Update()
-    {
-
-    }
 
     public void PlayHitParticle(Vector3 position)
     {
